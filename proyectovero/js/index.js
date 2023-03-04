7045fd18b5d6e6aa56f1185700d64176aa35ca37
VeroCeliz <126921890+VeroCeliz@users.noreply.github.com>
2023-03-03 21:42:59 -0300

    Delete proyectovero directory

--- proyectovero/js/index.js
+++
@@ -1,9 +0,0 @@
-// Aquí inserto el botón con comportamiento
-
-document.getElementById('boton')  .addEventListener ('click',function () {
-    console.log("Ahora aparecerá un mensaje en la consola");
-    document.getElementById('demo').innerHTML ="Probando el botón de mi CV";
-
-}
-
-)
